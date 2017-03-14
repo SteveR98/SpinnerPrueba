01598e4b2951ab410afe60d6ad9d85c5f3496ffc
SteveR98 <alvarodava@gmail.com>
2017-03-14 20:16:58 +0100

    Spinner

---
+++ app/src/main/java/val/femxa/cam/edu/spinnerprueba/MainActivity.java
@@ -0,0 +1,52 @@
+package val.femxa.cam.edu.spinnerprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView lblMensaje;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lblMensaje = (TextView)findViewById(R.id.LblMensaje);
+        spinner = (Spinner)findViewById(R.id.CmbOpciones);
+
+        final String[] datos =
+                new String[]{"ANIMALES","DEPORTES","EÑEPALABRAS","FAMOSOS","FRUTA","LUGARES","PELICULAS"};
+
+        //Alternativa 1: Array java
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_item, datos);
+
+        adaptador.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adaptador);
+
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent,
+                                               android.view.View v, int position, long id) {
+                        lblMensaje.setText("Categoria Seleccionada: " +
+                                parent.getItemAtPosition(position));
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        lblMensaje.setText("");
+                    }
+                });
+    }
+
+}
+
